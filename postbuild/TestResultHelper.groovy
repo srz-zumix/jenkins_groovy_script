28224e6419a657ec9442e64ff95777e938e4a0f5
srz_zumix <zumix.cpp@gmail.com>
2016-01-09 17:39:23 +0900

    getTestResultAction deprecated

--- postbuild/TestResultHelper.groovy
+++ postbuild/TestResultHelper.groovy
@@ -1,17 +1,21 @@
+import hudson.tasks.test.AbstractTestResultAction
 
 static addTestResultDescription(manager)
 {
-  def act = manager.build.getTestResultAction()
-  if( act != null ) {
-    def tr = act.getResult()
-    tr.description = "built on ${manager.build.getBuiltOn().getNodeName()}"
-    def st = tr.getSuites()
-    st.each { suite ->
-      def cr = suite.getCases()
-      cr.each { res ->
-        res.description = "${manager.build.getTime().format("MM/dd(E) HH:mm::ss")}"
-      }
-    }
-  }
+	//def act = manager.build.getTestResultAction()
+	def act = manager.build.getAction(AbstractTestResultAction.class)
+	if( act == null ) {
+		manager.listener.logger.println "testResultAction not found"
+		return
+	}
+	def tr = act.getResult()
+	tr.description = "built on ${manager.build.getBuiltOn().getNodeName()}"
+	def st = tr.getSuites()
+	st.each { suite ->
+		def cr = suite.getCases()
+		cr.each { res ->
+			res.description = "${manager.build.getTime().format("MM/dd(E) HH:mm::ss")}"
+		}
+	}
 }
 
